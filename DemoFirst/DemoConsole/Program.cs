f90e104a0a8b3ab25b99781e8ac568650d1885b7
Cyril Vincent <contact@cyrilvincent.com>
2024-12-09 09:26:31 +0100

    ca

--- DemoFirst/DemoConsole/Program.cs
+++ DemoFirst/DemoConsole/Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!!!!!");
 int i = 1;
-i = i + 1;
+int j = i++;
+int k = ++i;
+k = ++i * j++ - 2;
+
+long l = i;
+j = (int)l;
+
+Console.WriteLine($"{i},{j},{k}");
 Console.ReadLine();
